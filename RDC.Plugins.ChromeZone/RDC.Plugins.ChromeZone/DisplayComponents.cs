dcc2d1522779121a6871546b8435bce911593610
Justin Campbell <justin.campbell@lexel.co.nz>
2018-07-24 16:12:56 +1200

    -Working Example of Chrome as a tab in Ozone

---
+++ RDC.Plugins.ChromeZone/RDC.Plugins.ChromeZone/DisplayComponents.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDC.Plugins.ChromeZone
+{
+    public partial class DisplayComponents : Origen.Ozone.Plugins.OzonePlugin
+    {
+        public System.Windows.Forms.ToolStripContainer toolStripContainer;
+        public System.Windows.Forms.ToolStrip toolStrip1;
+        public System.Windows.Forms.ToolStripButton backButton;
+        public System.Windows.Forms.ToolStripButton forwardButton;
+        public ToolStripTextBox urlTextBox;
+        public System.Windows.Forms.ToolStripButton goButton;
+        public System.Windows.Forms.Label outputLabel;
+        public System.Windows.Forms.Label statusLabel;
+        public ToolStripButton refreshButton;
+        public ToolStripButton homeButton;
+        public ToolStripSeparator toolStripSeparator1;
+
+
+        public void InitializeComponent()
+        {
+            this.toolStripContainer = new System.Windows.Forms.ToolStripContainer();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.backButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.forwardButton = new System.Windows.Forms.ToolStripButton();
+            this.urlTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.refreshButton = new System.Windows.Forms.ToolStripButton();
+            this.homeButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripContainer.ContentPanel.SuspendLayout();
+            this.toolStripContainer.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer
+            // 
+            // 
+            // toolStripContainer.ContentPanel
+            // 
+            this.toolStripContainer.ContentPanel.Controls.Add(this.statusLabel);
+            this.toolStripContainer.ContentPanel.Controls.Add(this.outputLabel);
+            this.toolStripContainer.ContentPanel.Size = new System.Drawing.Size(730, 465);
+            this.toolStripContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer.LeftToolStripPanelVisible = false;
+            this.toolStripContainer.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer.Name = "toolStripContainer";
+            this.toolStripContainer.RightToolStripPanelVisible = false;
+            this.toolStripContainer.Size = new System.Drawing.Size(730, 490);
+            this.toolStripContainer.TabIndex = 0;
+            this.toolStripContainer.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer.TopToolStripPanel
+            // 
+            this.toolStripContainer.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusLabel.Location = new System.Drawing.Point(0, 439);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 13);
+            this.statusLabel.TabIndex = 1;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.outputLabel.Location = new System.Drawing.Point(0, 452);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeButton,
+            this.backButton,
+            this.toolStripSeparator1,
+            this.forwardButton,
+            this.refreshButton,
+            this.urlTextBox});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0);
+            this.toolStrip1.Size = new System.Drawing.Size(730, 25);
+            this.toolStrip1.Stretch = true;
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // backButton
+            // 
+            this.backButton.BackColor = System.Drawing.Color.Transparent;
+            this.backButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.backButton.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(23, 22);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.BackColor = System.Drawing.Color.Transparent;
+            this.forwardButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.forwardButton.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(23, 22);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.AutoSize = false;
+            this.urlTextBox.ReadOnly = true;
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(800, 25);
+            // 
+            // toolStripButton1
+            // 
+            this.refreshButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.refreshButton.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(23, 22);
+
+
+            this.homeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.homeButton.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(23, 22);
+
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.toolStripContainer);
+            this.Size = new System.Drawing.Size(730, 490);
+            this.toolStripContainer.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer.ContentPanel.PerformLayout();
+            this.toolStripContainer.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer.ResumeLayout(false);
+            this.toolStripContainer.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+
+            //Setup the icons for the buttons
+            Assembly asm = Assembly.GetExecutingAssembly();
+            Bitmap backButtonImage = new Bitmap(asm.GetManifestResourceStream("RDC.Plugins.ChromeZone.Icons.arrow-left.png"));
+            Bitmap forwardButtonImage = new Bitmap(asm.GetManifestResourceStream("RDC.Plugins.ChromeZone.Icons.arrow-right.png"));
+            Bitmap refreshButtonImage = new Bitmap(asm.GetManifestResourceStream("RDC.Plugins.ChromeZone.Icons.refresh.png"));
+            Bitmap homeButtonImage = new Bitmap(asm.GetManifestResourceStream("RDC.Plugins.ChromeZone.Icons.home.png"));
+
+            this.backButton.Image = backButtonImage;
+            this.forwardButton.Image = forwardButtonImage;
+            this.refreshButton.Image = refreshButtonImage;
+            this.homeButton.Image = homeButtonImage;
+        }
+
+    }
+}
---
+++ RDC.Plugins.ChromeZone/RDC.Plugins.ChromeZone/WebDisplay.cs
@@ -0,0 +1,143 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using CefSharp.WinForms.Internals;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDC.Plugins.ChromeZone
+{
+    public partial class WebDisplay : DisplayComponents
+    {
+        private ChromiumWebBrowser cBrowser;
+
+        static string lib, browserExe, locales, res;
+
+        private string AddressURL = "";
+
+        public WebDisplay()
+        {
+            InitializeComponent();
+            SetupBrowser();
+
+            forwardButton.Click += forwardButton_Click;
+            backButton.Click += backButton_Click;
+            refreshButton.Click += toolStripButton1_Click;
+            homeButton.Click += HomeButton_Click;
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            cBrowser.Load(BuildURL());
+        }
+
+        //Ozone Initialize function
+        public override void Initialize(string InitData)
+        {
+            AddressURL = InitData;
+            cBrowser.Load(BuildURL());
+        }
+
+        public override void RecordRefresh(string InitData = "")
+        {
+            cBrowser.Load(BuildURL());
+        }
+
+        //Setup the browser and it's dll directories 
+        private void SetupBrowser()
+        {
+            lib = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"cefsharp\libcef.dll");
+            browserExe = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"cefsharp\CefSharp.BrowserSubprocess.exe");
+            locales = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"cefsharp\locales\");
+            res = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"cefsharp\");
+
+            var libraryLoader = new CefLibraryHandle(lib);
+
+            var settings = new CefSettings();
+            settings.BrowserSubprocessPath = browserExe;
+            settings.LocalesDirPath = locales;
+            settings.ResourcesDirPath = res;
+
+            Cef.Initialize(settings);
+
+            cBrowser = new ChromiumWebBrowser("about:blank")
+            {
+                Dock = DockStyle.Fill,
+            };
+            toolStripContainer.ContentPanel.Controls.Add(cBrowser);
+
+            cBrowser.AddressChanged += OnBrowserAddressChanged;
+        }
+
+        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs args)
+        {
+            this.InvokeOnUiThreadIfRequired(() => urlTextBox.Text = args.Address);
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            cBrowser.Back();
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            cBrowser.Forward();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            cBrowser.Reload();
+        }
+
+        public string BuildURL()
+        {
+            string URL = AddressURL;
+
+            URL = URL.Replace("@@BusinessObjectID@@", BusinessObjectID);
+            URL = URL.Replace("@@RecordID@@", RecordID);
+            URL = URL.Replace("@@ScreenID@@", ScreenID);
+
+            return URL;
+        }
+
+        private string BusinessObjectID
+        {
+            get
+            {
+                if (this.CommonBlock != null && this.CommonBlock.CurrentRecord != null)
+                {
+                    return this.CommonBlock.CurrentRecord.FileId;
+                }
+                return "";
+            }
+        }
+
+        private string RecordID
+        {
+            get
+            {
+                if (this.CommonBlock != null && this.CommonBlock.CurrentRecord != null)
+                {
+                    return this.CommonBlock.CurrentRecord.RecordId;
+                }
+                return "";
+            }
+        }
+
+        private string ScreenID
+        {
+            get
+            {
+                if (this.CommonBlock != null && this.CommonBlock.CurrentRecord != null)
+                {
+                    return this.CommonBlock.Function;
+                }
+                return "";
+            }
+        }
+    }
+}
